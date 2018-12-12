c2af73d432f1e6028eef79f4a08525335db8c628
EC2 Default User <ec2-user@ip-172-31-11-143.us-east-2.compute.internal>
2018-12-12 00:48:46 +0000

    new updates

--- app.js
+++ app.js
@@ -12,11 +12,39 @@ app.use(express.static(__dirname + '../public'));
 app.get("/",function(req,res){
     res.render("home");
 });
+
+//create schema for mongodb to store mastery info.
+mongoose.connect("mongodb://localhost/LOLStats");
+var masterySchema = mongoose.Schema({
+    userID: String,
+    championID: String,
+    championLevel: String,
+    lastPlayTime: String,
+});
+var mas = mongoose.model("mastery",masterySchema);
+
 app.get("/mastery",function(req,res){
-    request("https://na1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/100010686?api_key=RGAPI-72907cb3-540c-4a35-bee0-6ff8cd9a4270",function(error,response,body){
+    request("https://na1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/100010686?api_key=RGAPI-f754480e-8116-4abe-bbff-09dff020e67b",function(error,response,body){
         if(!error){
             var parsedbd = JSON.parse(body);
             res.render("mastery.ejs",{parsedbd:parsedbd});
+            //send the mastery to mongod db
+            for (var i=0;i<parsedbd.length;i++){
+                var m = new mas({
+                   userID: parsedbd[i]["playerId"],
+                   championID: parsedbd[i]["championID"],
+                   championLevel: parsedbd[i]["championLevel"],
+                   lastPlayTime: parsedbd[i]["lastPlayTime"]
+                });
+                m.save(function(error,ret){
+                    if(error){
+                        console.log("Failed to save mastery to database");
+                    }else{
+                        console.log("We just save a mastery:");
+                        console.log(ret);
+                    }
+                });
+            }
         }else{
             res.send(error);
         }
